95fd4ecc8dd89e9aa405020f4c3d88c458db4cb7
Karel <karel.hartlieb@gmail.com>
2017-05-02 14:27:12 -0500

    first two functions working

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], function(err, result) {
+      cb(result);
+    });
+  },
+  insertOne: function(tableInput, col, valOfCol, cb) {
+    var queryString = "INSERT INTO ?? (??) VALUES (?)";
+    console.log(col)
+    console.log(valOfCol);
+
+    connection.query(queryString, [tableInput, col, valOfCol], function(err, result) {
+      cb(result);
+    });
+  },
+  updateOne: function(tableInput, col, condition, cb) {
+    var queryString = "UPDATE ?? SET devoured = ? WHERE id = ?";
+
+    console.log(col);
+    //console.log(valOfCol);
+    console.log(condition);
+    
+    connection.query(queryString, [tableInput, col, condition], function(err, result) {
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,33 @@
+//Dependencies
+//Require express
+var express = require("express");
+//Require body-parser
+var bodyParser = require("body-parser");
+//Require method-override
+var methodOverride = require("method-override");
+//Require express-handlebars
+var exphbs = require("express-handlebars");
+
+var port = 3000;
+
+var app = express();
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=PUT
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
