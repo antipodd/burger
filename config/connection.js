970bfe7124cf3a382a32946c236955365bf6eaf1
Karel <karel.hartlieb@gmail.com>
2017-05-04 12:51:38 -0500

    heroku 4

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    });
+};
+
+/*var connection = mysql.createConnection({
   port: 3306,
   host: "jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "qvfqlmyiges8tcjz",
   password: "wq6cfp7ku2qkkfa7",
   database: "zjbtg62d7gn9bg7s"
-});
+});*/
 
 //var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
